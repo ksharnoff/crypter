a0b4124a2fd4f5d2d388115f2435cceb08df7a0a
Kezia Sharnoff <105600498+ksharnoff@users.noreply.github.com>
2024-08-28 18:26:16 -0700

    Rewrote code, error messages

--- crypter.go
+++ crypter.go
@@ -1,3 +1,21 @@
+/*
+This program decrypts or encrypts a file with AES 256 encryption. It uses a
+single salt that is predetermined, as written in the function named
+keyGeneration.
+
+A single salt for all encryption has the upside that it is impossible to lose
+the salt, something that would make the file unable to be decrypted.
+Theoretically, the downside is that when re-encrypting the same file, a 
+malicious actor can see where any changes were. However, there is randomness 
+in this encryption so it does not have that downside.
+
+This can take in a file of any type. The encryption will be written to a .txt 
+file. Then when decrypted, it will turn back to its original file type. If the 
+inputted encrypted file starts off at .yaml, the decrypted will remain .yaml. 
+This behavoir to do with .yaml is because of my password manager, 
+github.com/ksharnoff/pass.
+*/
+
 package main
 
 import (
@@ -32,16 +50,15 @@ func main() {
 
 	for fileName == "0" {
 		skipLine()
-		fmt.Println("Make sure the file is in the same folder as this program!\nJust write exactly what the file is named\nMake sure that you include the extension!\n\taka 'testing.pdf' not just 'testing'")
+		fmt.Println("Make sure the file is in the same folder as this program!\nJust write exactly what the file is named\nMake sure that you include the extension!\n\taka 'testing.pdf' not just 'testing'\nAlso make sure that there is no spaces in the file name!\n")
 		skipLine()
 		fmt.Print("What is the name of the file?: ")
 		fmt.Scan(&fileName)
 	}
 	if doesFileExist(fileName) {
-		fmt.Println("Woo that file exists!")
+		fmt.Println("That file exists!")
 	} else {
-		fmt.Println("Wopp wopp that file does not exist")
-		os.Exit(1)
+		printAndExit("Error! The chosen file does not exist")
 	}
 	// from this point on, the file chosen does exist
 
@@ -52,7 +69,7 @@ func main() {
 		fmt.Scan(&action)
 		if action == 0 {
 			skipLine()
-			fmt.Print("Decrypt means to decode, make it a readable file.\nEncrypt does the opposite, it scrambles it.")
+			fmt.Print("Decrypt means to decode, make it a readable file.\nEncrypt does the opposite, it scrambles it.\nIf You chose decrypt and the file is not encrypted, the program will fail.")
 		}
 	}
 
@@ -79,26 +96,17 @@ func main() {
 
 	fmt.Println("THINGS ARE HAPPENING - DO NOT QUIT THE PROGRAM")
 
-	input, inputErr := os.ReadFile(fileName) // input is an [] of bytes
+	input, inputErr := os.ReadFile(fileName) // input is a [] of bytes
 
-	if inputErr != nil { // if error in reading from file, ex: file doesn't exist
-		fmt.Print("Error in os.ReadFile " + inputErr.Error())
-		os.Exit(1)
+	if inputErr != nil {
+		printAndExit("Error in os.ReadFile " + inputErr.Error())
 	}
 
 	if len(input) == 0 {
-		fmt.Println("That file is empty! Error!!")
-		os.Exit(0)
+		printAndExit("Error! That file is empty")
 	}
 
-	// to add -- salt generation at this point
-
-	ciphBlock, keyErr := keyGeneration(password)
-
-	if keyErr != "" {
-		fmt.Print("Errror in key generation " + keyErr)
-		os.Exit(1)
-	}
+	ciphBlock := keyGeneration(password)
 
 	// assuming fileName input was testingtest.pdf
 	// new encrypted file should be: e-testingtest.pdf.txt
@@ -107,19 +115,17 @@ func main() {
 	var outputToWrite []byte
 	var newFileName string
 
-	// decrypts or encrypts
-	if action == 1 { // should decrypt
+	if action == 1 { // decrypts
 		outputToWrite = decrypt(input, ciphBlock)
 		newFileName = "d"
-	} else if action == 2 { // should encrypt
+	} else if action == 2 { // encrypts
 		outputToWrite = encrypt(input, ciphBlock)
 		newFileName = "e"
 	} else { // should never be this!
-		fmt.Println("Problem! Didn't chose decrypt/encrypt!!")
-		os.Exit(1)
+		printAndExit("Error! Encrypt or decrypt were not chosen")
 	}
 
-	newFileName += "-" + fileName
+	newFileName += fmt.Sprint("-" + fileName)
 
 	// if the newFileName already exists then it adds a random two digits to the start
 	// so it would look like: 38e-testingtest.pdf.txt
@@ -127,23 +133,22 @@ func main() {
 		newFileName = strconv.Itoa(randNum()) + newFileName // adds a random two digits to the start
 	}
 
-	if action == 1 { // if decrypting, remove the .txt added when encrypted, making the file go back to its original status of .pdf or etc
+	if action == 1 { // if decrypting, remove the .txt added when encrypted, making the file go back to its original status of .pdf, etc
 		beforeTxt, foundTxt := strings.CutSuffix(newFileName, ".txt")
 		if foundTxt { // if newFileName did end in ".txt"
 			newFileName = beforeTxt
 		} else { // it shouldn't get to this else, if it was encrypted by crypter!
-			_, foundYaml := strings.CutSuffix(newFileName, ".yaml") // if was encrypted by my pass manager will be testing.yaml, this will leave it as a .yaml
+			_, foundYaml := strings.CutSuffix(newFileName, ".yaml") // if was encrypted by my pass manager will be 'testing.yaml', this will leave it as .yaml
 
 			if !foundYaml {
-				fmt.Println("Are you sure that you want to Decrypt? Decyrypt (1) or encrypt (2)?: ")
+				fmt.Println("Are you sure that you meant to Decrypt? Decyrypt (1) or encrypt (2)?: ") // because at this point it will have already decrypted
 				fmt.Scan(&action)
 
 				if action == 1 {
 					newFileName += ".txt"
 					fmt.Println("THINGS ARE HAPPENING - DO NOT QUIT THE PROGRAM")
 				} else {
-					fmt.Println("Start over!")
-					os.Exit(1)
+					printAndExit("Error! You changed your mind about decrypting")
 				}
 			}
 		}
@@ -151,22 +156,28 @@ func main() {
 		newFileName += ".txt"
 	}
 
-	writeErr := os.WriteFile(newFileName+".tmp", outputToWrite, 0600) // 0600 is  permissions that only this user can read/write/excet to this file
+	writeErr := os.WriteFile(newFileName + ".tmp", outputToWrite, 0600) // 0600 is  permissions that only this user can read/write/excet to this file
 
 	if writeErr != nil {
-		fmt.Println("Error in os.WriteFile " + writeErr.Error())
+		printAndExit("Error in os.WriteFile " + writeErr.Error())
 	}
 
-	os.Rename(newFileName+".tmp", newFileName) // only will do this if previous writing worked
+	os.Rename(newFileName + ".tmp", newFileName) // only will do this if previous writing worked
 
 	fmt.Println("All is done!")
+
 	if action == 1 { // if decrypting
-		fmt.Println("Btw, if you got the password wrong, the file will be gibberish!")
+		fmt.Println("By the way, if you got the password wrong, the file will be gibberish!")
 	}
 }
 
 func skipLine() {
 	fmt.Print("\n")
+}
+
+func printAndExit(error string) {
+	fmt.Println(error)
+	os.Exit(1)
 }
 
 // returns bool for if a file exists (within the
@@ -184,29 +195,28 @@ func randNum() int {
 	return mathRand.Intn(99) // should return number less than 99
 }
 
-// Makes a key, then a cipher block. returns "" if the
-// password generation was successfull.
-func keyGeneration(password string) (cipher.Block, string) {
+// Makes a key, then a cipher block.
+func keyGeneration(password string) cipher.Block {
 	if len([]byte(password)) < 1 {
-		return nil, "password for key generation is too short, string empty"
+		printAndExit("Error! The password for key generation is too short")
 	}
 
-	// Salt generation is going to be the same thing every time. TO DO - CHANGE
+	// The salt is going to be the same every time.
 	salt := []byte("zxcvbnmlkjhgfdsa")
 
 	// pass params: 4, 2048*1024, 4, 32 -- takes about 2 seconds
-	// func IDKey(password, salt []byte, time, memory uint32, threads uint8, keyLen uint32)
 	// crypter params: 2, 64*1024, 4, 32
 	key := argon2.IDKey([]byte(password), salt, 2, 64*1024, 4, 32)
 
 	ciphBlock, err := aes.NewCipher(key)
 
 	if err != nil {
-		return nil, err.Error()
+		printAndExit("Error in key generation!" + err.Error())
 	}
-	return ciphBlock, ""
+	return ciphBlock
 }
 
+// returns an encrypted slice of bytes
 func encrypt(plaintext []byte, ciphBlock cipher.Block) []byte {
 	// adds padding in form of "\n"
 	if len(plaintext)%aes.BlockSize != 0 {
@@ -214,13 +224,16 @@ func encrypt(plaintext []byte, ciphBlock cipher.Block) []byte {
 			plaintext = append(plaintext, 0x0A) // 0x0A = []byte("\n")
 		}
 	}
-	encrypt := make([]byte, aes.BlockSize+len(plaintext))
+	encrypt := make([]byte, aes.BlockSize + len(plaintext))
 
 	iv := encrypt[:aes.BlockSize]
 
-	if _, err := io.ReadFull(cryptRand.Reader, iv); err != nil {
-		panic(err)
+	_, err := io.ReadFull(cryptRand.Reader, iv)
+
+	if err != nil {
+		printAndExit("Error in crypto rand IV generation! " + err.Error())
 	}
+
 	encryptBlock := cipher.NewCBCEncrypter(ciphBlock, iv)
 
 	encryptBlock.CryptBlocks(encrypt[aes.BlockSize:], plaintext)
@@ -228,6 +241,8 @@ func encrypt(plaintext []byte, ciphBlock cipher.Block) []byte {
 	return encrypt
 }
 
+
+// returns an unencrypted slice of bytes
 func decrypt(encrypted []byte, ciphBlock cipher.Block) []byte {
 	iv := encrypted[:aes.BlockSize]
 	encrypted = encrypted[aes.BlockSize:]
